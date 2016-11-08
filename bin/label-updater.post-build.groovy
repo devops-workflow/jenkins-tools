e7b7e24e6188a23b65384637c9c1d05e58abc787
Steven Nemetz <snemetz@roku.com>
2016-11-08 08:25:57 -0800

    Changing to work with groovy build step

--- bin/label-updater.post-build.groovy
+++ bin/label-updater.post-build.groovy
@@ -1,29 +1,29 @@
-nodeName = build.buildVariableResolver.resolve('NodeToUpdate')
-labelName = build.buildVariableResolver.resolve('LabelName')
-set = build.buildVariableResolver.resolve('DesiredState')
+nodeName = manager.envVars['NodeToUpdate']
+labelName = manager.envVars['LabelName']
+set = manager.envVars['DesiredState']
 
 for (node in jenkins.model.Jenkins.instance.nodes) {
     if (node.getNodeName().equals(nodeName)) {
-        listener.logger.println("Found node to update: " + nodeName)
+        manager.listener.logger.println("Found node to update: " + nodeName)
         oldLabelString = node.getLabelString()
         if (set.equals('true')) {
             if (!oldLabelString.contains(labelName)) {
-                listener.logger.println("Adding label '" + labelName     + "' from node " + nodeName);
+                manager.listener.logger.println("Adding label '" + labelName     + "' from node " + nodeName);
                 newLabelString = oldLabelString + " " + labelName
                 node.setLabelString(newLabelString)
                 node.save()
             } else {
-                listener.logger.println("Label '" + labelName + "' already exists on node " + nodeName)
+                manager.listener.logger.println("Label '" + labelName + "' already exists on node " + nodeName)
             }
         }
         else {
             if (oldLabelString.contains(labelName)) {
-                listener.logger.println("Removing label '" + labelName + "' from node " + nodeName)
+                manager.listener.logger.println("Removing label '" + labelName + "' from node " + nodeName)
                 newLabelString = oldLabelString.replaceAll(labelName, "")
                 node.setLabelString(newLabelString)
                 node.save()
             } else {
-                listener.logger.println("Label '" + labelName + "' doesn't exist on node " + nodeName)
+                manager.listener.logger.println("Label '" + labelName + "' doesn't exist on node " + nodeName)
             }
         }
     }
