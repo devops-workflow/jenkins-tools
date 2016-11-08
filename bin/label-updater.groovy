d9a8d58b4ee53925716d9ecd5bf890abab005343
Steven Nemetz <snemetz@roku.com>
2016-11-08 08:34:15 -0800

    Add debug statements

--- bin/label-updater.groovy
+++ bin/label-updater.groovy
@@ -1,6 +1,8 @@
 nodeName = build.buildVariableResolver.resolve('NodeToUpdate')
 labelName = build.buildVariableResolver.resolve('LabelName')
 set = build.buildVariableResolver.resolve('DesiredState')
+listener.logger.println("Running label updater")
+listener.logger.println("Node: " + nodeName + "Label: " + labelName + "Set: " + set)
 
 for (node in jenkins.model.Jenkins.instance.nodes) {
     if (node.getNodeName().equals(nodeName)) {
