4278da9e3cbcea41a306d75891dee9dcf8701855
Steven Nemetz <snemetz@wiser.com>
2018-03-07 09:38:35 -0800

    Fix fields. Fail on error

--- bin/test-service.sh
+++ bin/test-service.sh
@@ -3,11 +3,25 @@
 # Trigger automated test service
 #
 
+if [ "TEST" == "$1" ]; then
+  GIT_URL="https://github.com/WisePricer/proxy-service.git"
+  GIT_COMMIT="xxxxxxx"
+  GIT_COMMITTER_NAME="Steven"
+  BUILD_ID=1
+fi
+
 header="Content-Type: application/json"
 repo=$(basename ${GIT_URL})
 service="${repo%%.*}"
 gitCommitMsg=$(git log -1 --pretty=%B)
 
-curl --header "${header}" \
---data "{\"service\":\"${service}\", \"gitCommit\":\"${GIT_COMMIT}\", \"commitedBy\":\"${GIT_COMMITTER_NAME}\", \"commitMessage\":\"${gitCommitMsg}\", \"JenkinsBuildId\":\"${BUILD_ID}\", \"user\":\"jenkins\"}" \
+curl -fi --header "${header}" \
+--data "{\"service\":\"${service}\", \"gitCommit\":\"${GIT_COMMIT}\", \"commitedBy\":\"${GIT_COMMITTER_NAME}\", \"commitMessage\":\"${gitCommitMsg}\", \"jenkinsBuildId\":\"${BUILD_ID}\", \"user\":\"jenkins\"}" \
 http://tas.test.wiser.com:3060/test_deployed_service
+result=$?
+if [ $result -ne 0 ]; then
+  echo -e "\nERROR: curl command failed. Result: ${result}"
+  exit ${result}
+fi
+
+#--data "{\"service\":\"${service}\", \"gitCommit\":\"${GIT_COMMIT}\", \"commitedBy\":\"${GIT_COMMITTER_NAME}\", \"commitMessage\":\"${gitCommitMsg}\", \"JenkinsBuildId\":\"${BUILD_ID}\", \"user\":\"jenkins\"}" \
