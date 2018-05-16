469c507e7419204c61c4ebe44bbc19985bcbf197
Steven Nemetz <snemetz@wiser.com>
2018-05-16 09:12:51 -0700

    Add debugging to help determine why sometimes get 400 on curl run

--- bin/test-service.sh
+++ bin/test-service.sh
@@ -15,6 +15,9 @@ repo=$(basename ${GIT_URL})
 service="${repo%%.*}"
 gitCommitMsg=$(git log -1 --pretty=%B)
 
+data="{\"service\":\"${service}\", \"gitCommit\":\"${GIT_COMMIT}\", \"commitedBy\":\"${GIT_COMMITTER_NAME}\", \"commitMessage\":\"${gitCommitMsg}\", \"jenkinsBuildId\":\"${BUILD_ID}\", \"user\":\"jenkins\"}"
+echo "Data sending to TAS: ${data}"
+
 curl -fiSs --header "${header}" \
 --data "{\"service\":\"${service}\", \"gitCommit\":\"${GIT_COMMIT}\", \"commitedBy\":\"${GIT_COMMITTER_NAME}\", \"commitMessage\":\"${gitCommitMsg}\", \"jenkinsBuildId\":\"${BUILD_ID}\", \"user\":\"jenkins\"}" \
 http://tas.test.wiser.com:3060/test_deployed_service
