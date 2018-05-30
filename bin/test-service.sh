b5d870ed971646dc2b80488dd71c46e3e7529012
Steven Nemetz <snemetz@wiser.com>
2018-05-30 09:55:00 -0700

    Change to urlencode data

--- bin/test-service.sh
+++ bin/test-service.sh
@@ -13,13 +13,15 @@ fi
 header="Content-Type: application/json"
 repo=$(basename ${GIT_URL})
 service="${repo%%.*}"
+# Commit message can be multiple lines
 gitCommitMsg=$(git log -1 --pretty=%B)
 
 data="{\"service\":\"${service}\", \"gitCommit\":\"${GIT_COMMIT}\", \"commitedBy\":\"${GIT_COMMITTER_NAME}\", \"commitMessage\":\"${gitCommitMsg}\", \"jenkinsBuildId\":\"${BUILD_ID}\", \"user\":\"jenkins\"}"
 echo "Data sending to TAS: ${data}"
 
+# Change --data to --data-urlencode ? Test may not work
 curl -fiSs --header "${header}" \
---data "{\"service\":\"${service}\", \"gitCommit\":\"${GIT_COMMIT}\", \"commitedBy\":\"${GIT_COMMITTER_NAME}\", \"commitMessage\":\"${gitCommitMsg}\", \"jenkinsBuildId\":\"${BUILD_ID}\", \"user\":\"jenkins\"}" \
+--data-urlencode "{\"service\":\"${service}\", \"gitCommit\":\"${GIT_COMMIT}\", \"commitedBy\":\"${GIT_COMMITTER_NAME}\", \"commitMessage\":\"${gitCommitMsg}\", \"jenkinsBuildId\":\"${BUILD_ID}\", \"user\":\"jenkins\"}" \
 http://tas.test.wiser.com:3060/test_deployed_service
 result=$?
 if [ $result -ne 0 ]; then
