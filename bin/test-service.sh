486a41e34154983aeb72b94a0384876856629715
Steven Nemetz <snemetz@wiser.com>
2018-05-31 11:07:01 -0700

    Strip line endings. JSON parser cannot handle

--- bin/test-service.sh
+++ bin/test-service.sh
@@ -14,14 +14,14 @@ header="Content-Type: application/json"
 repo=$(basename ${GIT_URL})
 service="${repo%%.*}"
 # Commit message can be multiple lines
-gitCommitMsg=$(git log -1 --pretty=%B)
+gitCommitMsg=$(git log -1 --pretty=%B | tr -d '\r' | tr '\n' ' ')
 
 data="{\"service\":\"${service}\", \"gitCommit\":\"${GIT_COMMIT}\", \"commitedBy\":\"${GIT_COMMITTER_NAME}\", \"commitMessage\":\"${gitCommitMsg}\", \"jenkinsBuildId\":\"${BUILD_ID}\", \"user\":\"jenkins\"}"
 echo "Data sending to TAS: ${data}"
 
 # Change --data to --data-urlencode ? Test may not work
 curl -fiSs --header "${header}" \
---data-urlencode "{\"service\":\"${service}\", \"gitCommit\":\"${GIT_COMMIT}\", \"commitedBy\":\"${GIT_COMMITTER_NAME}\", \"commitMessage\":\"${gitCommitMsg}\", \"jenkinsBuildId\":\"${BUILD_ID}\", \"user\":\"jenkins\"}" \
+--data "{\"service\":\"${service}\", \"gitCommit\":\"${GIT_COMMIT}\", \"commitedBy\":\"${GIT_COMMITTER_NAME}\", \"commitMessage\":\"${gitCommitMsg}\", \"jenkinsBuildId\":\"${BUILD_ID}\", \"user\":\"jenkins\"}" \
 http://tas.test.wiser.com:3060/test_deployed_service
 result=$?
 if [ $result -ne 0 ]; then
@@ -30,5 +30,3 @@ if [ $result -ne 0 ]; then
 else
   echo -e "\n"
 fi
-
-#--data "{\"service\":\"${service}\", \"gitCommit\":\"${GIT_COMMIT}\", \"commitedBy\":\"${GIT_COMMITTER_NAME}\", \"commitMessage\":\"${gitCommitMsg}\", \"JenkinsBuildId\":\"${BUILD_ID}\", \"user\":\"jenkins\"}" \
